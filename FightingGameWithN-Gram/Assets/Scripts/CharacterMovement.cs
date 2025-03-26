b22843cbe108191bd8f1aeadb8077d32c09f915b
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-03-25 22:48:22 -0700

    make move left right possible

---
+++ FightingGameWithN-Gram/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private InputActionReference move;
+
+    private Vector2 moveDirection;
+
+    private void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, 0.0f);
+    }
+}
